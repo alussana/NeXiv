1580d38637c6dc5cb85c5e523c0c31f5050669f1
alussana <alessandro.lussana@protonmail.com>
2020-11-29 10:34:10 +0000

    updated NeXiv_config.sh, NeXiv_start.sh; added docker-compose_template.yml

--- NeXiv_config.sh
+++ NeXiv_config.sh
@@ -1,4 +1,2 @@
 ## Set username and pwd for nextcloud
-sed -s "s/nextcloud_user/$1/" docker-compose.yml
-sed -s "s/mynormalpwd/$2/" docker-compose.yml
-sed -s "s/myrootpwd/$3/" docker-compose.yml
+sed -s "s/nextcloud_user/$1/; s/mynormalpwd/$2/; s/myrootpwd/$3/" docker-compose_template.yml > docker-compose.yml
