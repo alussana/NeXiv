836a89fbbaca88157a34ed226cbbb08e305de0a7
alussana <alessandro.lussana@protonmail.com>
2022-03-28 22:27:19 +0100

    minor update before moving local repo

---
+++ NeXiv_volumes.sh
@@ -0,0 +1,2 @@
+docker volume create --opt type=volume --opt device=:/home/alessandro/Xiv/NeXiv/nextcloud nextcloud
+docker volume create --opt type=volume --opt device=:/home/alessandro/Xiv/NeXiv/db db
